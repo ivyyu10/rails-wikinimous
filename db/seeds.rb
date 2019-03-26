d0cb4c4e6d1c32ae01f2d6d49b11fd6574c300a6
Ivy Yu <ivys.yu@gmail.com>
2019-03-26 18:26:25 +0800

    rails wiki update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.delete_all
+10.times do
+  Article.create!(title: Faker::Quote.singular_siegler, content: Faker::Quote.famous_last_words)
+  puts "We have created #{Article.count} articles"
+end
